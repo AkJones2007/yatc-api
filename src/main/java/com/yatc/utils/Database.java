ae7931dbf2d5ba7ab39ab03d3e3762a7cc77bf8c
Alex <AkJones2007@gmail.com>
2017-10-11 20:43:19 -0400

    Adds Database class with ability to connect to the MySQL database

---
+++ src/main/java/com/yatc/utils/Database.java
@@ -0,0 +1,42 @@
+package com.yatc.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    // PRIVATE CONSTANTS
+    // String address
+    private final String ADDRESS = "jdbc:mysql://localhost/";
+    // String username
+    private final String USERNAME = "root";
+    // String password
+    private final String PASSWORD = "akj04210";
+    // String DB_NAME
+    private final String DB_NAME = "yatc";
+
+    // PRIVATE FIELDS
+    // connection
+    private Connection connection = null;
+
+    // CONSTRUCTORS
+    // default
+    public Database() {
+        connect();
+    }
+
+    // PUBLIC METHODS
+    // void connect()
+    public void connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(ADDRESS + DB_NAME, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
