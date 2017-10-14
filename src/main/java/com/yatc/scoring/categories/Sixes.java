d6228a996c8ba41f2e085d6fb7de73e8de5d3dee
Alex <AkJones2007@gmail.com>
2017-10-14 16:10:48 -0400

    Updates Sixes scoring category to utilize new DiceValueAnalyzer class

--- src/main/java/com/yatc/scoring/categories/Sixes.java
+++ src/main/java/com/yatc/scoring/categories/Sixes.java
@@ -1,19 +1,14 @@
 package com.yatc.scoring.categories;
 
 import com.yatc.scoring.ScoreCategory;
+import com.yatc.utils.DiceValueAnalyzer;
 
 public class Sixes extends ScoreCategory {
 
     public int calculateValue(int[] diceValues) {
-        int score = 0;
+        DiceValueAnalyzer diceAnalyzer = new DiceValueAnalyzer(diceValues);
 
-        for (int value : diceValues) {
-            if (value == 6) {
-                score += 6;
-            }
-        }
-
-        return score;
+        return diceAnalyzer.getValueOccuranceCount(6) * 6;
     }
 
 }
