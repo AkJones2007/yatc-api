61c96546249f41c00f1ec092dd281db8a14a3c01
Alex <AkJones2007@gmail.com>
2017-10-14 16:21:24 -0400

    Updates Three Of A Kind scoring category to utilize new DiceValueAnalyzer class

--- src/main/java/com/yatc/scoring/categories/ThreeOfAKind.java
+++ src/main/java/com/yatc/scoring/categories/ThreeOfAKind.java
@@ -1,39 +1,18 @@
 package com.yatc.scoring.categories;
 
 import com.yatc.scoring.ScoreCategory;
-import java.util.ArrayList;
+import com.yatc.utils.DiceValueAnalyzer;
 
 public class ThreeOfAKind extends ScoreCategory {
 
     public int calculateValue(int[] diceValues) {
-        ArrayList<Integer> uniqueValues = new ArrayList<Integer>();
-        ArrayList<Integer> occurances = new ArrayList<Integer>();
-        int highestOccuranceCount = 1;
-        int score = 0;
+        DiceValueAnalyzer diceAnalyzer = new DiceValueAnalyzer(diceValues);
 
-        for (int value : diceValues) {
-            if (uniqueValues.contains(value)) {
-                int i = uniqueValues.indexOf(value);
-                int occuranceCount = occurances.get(i) + 1;
-
-                if (occuranceCount > highestOccuranceCount) {
-                    highestOccuranceCount = occuranceCount;
-                }
-
-                occurances.set(i, occuranceCount);
-            } else {
-                uniqueValues.add(value);
-                occurances.add(1);
-            }
+        if (diceAnalyzer.getHighestOccuranceCount() >= 3) {
+            return diceAnalyzer.getSum();
+        } else {
+            return 0;
         }
-
-        if (highestOccuranceCount >= 3) {
-            for (int value : diceValues) {
-                score += value;
-            }
-        }
-
-        return score;
     }
 
 }
